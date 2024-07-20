f1865f95c2ab88d4c8d29e5734e04610591fa4c5
Sam Cutler <sam.cutler@protonmail.com>
2024-07-20 20:23:14 +0100

    remove env file and systemd stuff

--- systemd/bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env
-
-mkdir -p /opt/jinwonkim.art/images
-mkdir -p /opt/jinwonkim.art/styles
-mkdir -p /opt/jinwonkim.art/js
-mkdir -p /opt/jinwonkim.art/templates
-mkdir -p /opt/jinwonkim.art/thumbs
